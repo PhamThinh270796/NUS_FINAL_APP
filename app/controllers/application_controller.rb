6c0181e902e3d5113ee102d121f7760eb7db0eb1
phamthinh2707 <pham.thinh2707@gmail.com>
2019-03-26 09:26:41 +0700

    Fix i18n, alert in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
-
-  def after_sign_in_path_for(resources)
-    root_path
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
