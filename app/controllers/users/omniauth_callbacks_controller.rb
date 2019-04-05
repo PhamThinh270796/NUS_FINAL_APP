a550d94efdb4f4d20f96390f61dcbede2e864107
phamthinh2707 <pham.thinh2707@gmail.com>
2019-04-05 17:49:11 +0700

    Fix byebug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,7 +16,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"]
-      byebug
       redirect_to new_user_registration_url
     end
   end
