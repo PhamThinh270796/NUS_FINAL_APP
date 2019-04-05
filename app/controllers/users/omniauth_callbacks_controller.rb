dac4a73c202cae53dbc6ee66b2bad81c15262a1f
phamthinh2707 <pham.thinh2707@gmail.com>
2019-04-05 13:18:43 +0700

    Fix duplicate code, typos

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,21 +1,22 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  before_action :handle_social_callback
-
   def google_oauth2
+    handle_social_callback("google_oauth2")
   end
 
   def facebook
+    handle_social_callback("facebook")
   end
 
   private
 
-  def handle_social_callback
+  def handle_social_callback(provider)
     @user = UserProvider.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+      byebug
       redirect_to new_user_registration_url
     end
   end
--- app/models/user_provider.rb
+++ app/models/user_provider.rb
@@ -2,25 +2,22 @@ class UserProvider < ApplicationRecord
   belongs_to :user
 
   def self.from_omniauth(auth)
-    user = UserProvider.where(:provider => auth.provider, :uid => auth.uid).first
-    unless user.nil?
-      user.user
+    user_provider = UserProvider.where(provider: auth.provider, uid: auth.uid).first
+    unless user_provider.nil?
+      user_provider.user
     else
-      registered_user = User.where(:email => auth.info.email).first
-      unless registered_user.nil?
-        UserProvider.create!(provider: auth.provider, uid: auth.uid, user_id: registered_user.id)
-        registered_user
-      else
+      user = User.where(email: auth.info.email).first
+      if user.nil?
         user = User.create!(
           first_name: auth.info.name.partition(" ").first,
           last_name: auth.info.name.partition(" ").last,
           email: auth.info.email,
           password: Devise.friendly_token[0,20],
           remote_profile_picture_url: auth.info.image
-          )
-        user_provider = UserProvider.create!(provider:auth.provider, uid:auth.uid, user_id: user.id)
-        user
+        )
       end
+      user.user_providers.create!(provider: auth.provider, uid: auth.uid, user_id: user.id)
+      user
     end
   end
 end
